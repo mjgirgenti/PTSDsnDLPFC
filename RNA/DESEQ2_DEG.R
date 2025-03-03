f9fc122f7e4f6fd20dc828e47238c427b34b4f9c
Siwei Xu <s.xu@uci.edu>
2025-03-03 13:30:59 -0800

    Update DESeq2 script to reflect changes in revision

--- RNA/DESEQ2_DEG.R
+++ RNA/DESEQ2_DEG.R
@@ -1,93 +1,135 @@
-library(MAST)
 library(Seurat)
 library(SeuratDisk)
-library(ggplot2)
-library(EnhancedVolcano)
-library(glue)
-library(data.table)
-library(stringr)
-library(edgeR)
-library(dplyr)
 library(DESeq2)
-library(R.utils)
-library(argparse)
-library(glmGamPoi)
-library(compositions)
+library(tidyverse)
+library(reshape2)
+library(edgeR)
+library(glue)
 
 parser <- ArgumentParser()
 parser$add_argument('--condition', type='character')
 parser$add_argument('--celltype', type='character')
 args <- parser$parse_args()
 
-cond <- args$condition
+condition <- args$condition
 celltype <- args$celltype
 
-data <- LoadH5Seurat(glue('/home/ah2428/ShareZhangLab/PTSD/RNA/DEG_data/{cond}_vs_CON/{cond}_CON_{celltype}.h5seurat'),assays='RNA',meta.data=FALSE)
-meta <- fread(glue('/home/ah2428/ShareZhangLab/PTSD/RNA/DEG_data/{cond}_vs_CON/{cond}_CON_{celltype}_meta.txt'))
+# Load data and metadata
+seurat_object <- LoadH5Seurat(glue("../PTSD/RNA/DEG_data/{condition}_vs_CON/{condition}_CON_{celltype}.h5seurat"), assays = "RNA", 
+                              meta.data = FALSE, misc = FALSE) 
+meta <- read.csv(file = glue("../PTSD/RNA/DEG_data/{condition}_vs_CON/{condition}_CON_{celltype}_meta.txt"),
+                 sep = '\t',row.names='barcodekey')
+seurat_object <- AddMetaData(object = seurat_object, metadata = meta)
+seurat_object$samples <- paste0(seurat_object$Condition, '.', seurat_object$Channel)
+seurat_object <- NormalizeData(seurat_object)
+celltype_to_use <- unique(seurat_object$class)
 
-protein <- fread('/home/ah2428/ShareZhangLab/CommonData/protein_coding.genes.with.chr.txt',header=FALSE)$V2
-protein_genes <- intersect(rownames(data),protein)
-data <- data[protein_genes,]
-print(dim(data))
+# Assign sample names to object
+Idents(seurat_object) <- paste(seurat_object$Condition, seurat_object$class, sep = "_")
+cts <- AggregateExpression(seurat_object, 
+                           group.by = c("class", "samples"),
+                           assays = 'RNA',
+                           slot = "counts",
+                           return.seurat = FALSE)
+cts <- cts$RNA
 
-counts <- data$RNA@counts
-counts <- as(counts, "dgCMatrix")
-bin_counts <- (counts > 0) 
-num_con <- floor(sum(meta$Condition == 'CON') * 0.2)
-num_cond <- floor(sum(meta$Condition == cond) * 0.2)
-con_sums <- rowSums(bin_counts[, which(meta$Condition=='CON')])
-cond_sums <- rowSums(bin_counts[, which(meta$Condition==cond)])
-df <- data.frame(con=con_sums,cond=cond_sums)
-filtered_genes <- rownames(df[df$con >= num_con | df$cond >= num_cond,])
-data2 <- data[filtered_genes,]
+# Split into a list of PseudoBulk Matrix per cell type
+cts.t <- t(cts)  # transpose
+cts.t <- as.data.frame(cts.t)  # convert to data.frame
+splitRows <- gsub('_.*', '', rownames(cts.t))  # get values where to split
+cts.split <- split.data.frame(cts.t, f = factor(splitRows))  # split data.frame
 
-data2@meta.data$Sample <- sapply(strsplit(rownames(data2@meta.data), "-"), head, 1)
-samples <- unique(data2@meta.data$Sample)
-remove <- samples[table(data2@meta.data$Sample) < 30]
-Idents(data2) <- data2@meta.data$Sample
-keep = setdiff(samples,remove)
-data2 <- subset(data2,idents = keep)
-print(dim(data2))
+# fix colnames and transpose
+cts.split.modified <- lapply(cts.split, function(x){
+    rownames(x) <- gsub('.*_(.*)', '\\1', rownames(x))
+    t(x)
+})
 
-counts <- data.frame(data2$RNA@counts)
-strings <- colnames(counts)
-new_colnames <- sapply(strsplit(strings,'[.]'),`[`,1)
-colnames(counts) <- new_colnames
-samples <- unique(colnames(counts))
-counts_df <- sapply(split.default(counts, colnames(counts)), rowSums)
+counts_cellType <- cts.split.modified
 
-meta2 <- meta[meta$Channel %in% samples,]
-meta2 <- meta2[,c('Channel','Condition','Sex','PMI','RIN','Race','AgeDeath')]
-meta2 <- meta2[!duplicated(meta2$Channel), ]
-meta2 <- meta2[,c('Condition','Sex','PMI','RIN','Race','AgeDeath')]
+samples_table <- table(seurat_object@meta.data$class, seurat_object@meta.data$Channel)
+samples_df <- as.data.frame(samples_table)
+samples_df <- dcast(samples_df, Var1 ~ Var2, value.var = "Freq")
+rownames(samples_df) <- samples_df$Var1
+samples_df$Var1 <- NULL
 
-meta2$Condition <- relevel(as.factor(meta2$Condition),'CON')
-meta2$Sex<- relevel(as.factor(meta2$Sex),'M')
-meta2$Race <- as.factor(meta2$Race)
-meta2$AgeDeath <- scale(as.numeric(meta2$AgeDeath))
-meta2$RIN <- scale(as.numeric(meta2$RIN))
-meta2$PMI <- scale(as.numeric(meta2$PMI))
+samples_cellType <- as.vector(samples_df)
 
-cdr <- log2(colSums(counts_df))
-meta2$cdr <- scale(as.numeric(cdr))
+remove_samples <- c()
+for (i in 1:length(samples_cellType)) {
+    sample = samples_cellType[i]
+    if (samples_cellType[[i]] < 30) {
+        remove_samples <- c(remove_samples, names(samples_cellType)[[i]])
+    }
+}
 
-dds <- DESeqDataSetFromMatrix(countData = counts_df, colData = meta2, design = ~ cdr + AgeDeath + PMI + RIN + Sex + Race + Condition)
-dds <- DESeq(dds, test = "LRT", useT = TRUE, reduced = ~ cdr + AgeDeath + PMI + RIN + Sex + Race, 
-             minmu = 1e-6, minReplicatesForReplace = Inf, fitType = 'glmGamPoi')
+if ('MDD' %in% seurat_object$Condition) {
+    sums <- sort(rowSums(cts.t))
+    sums <- data.frame(sums)
+    sums$cond <- substr(rownames(sums), 0, 3)
+    sums <- sums[sums$cond == 'MDD',]
+    samples <- rownames(sums)[1:7]
 
-res <- results(dds)
-write.table(res,glue('/home/ah2428/ShareZhangLab/ah2428/PTSD/deseq2_final/{celltype}_{cond}_DEG.csv'),sep='\t')
+    sums <- sort(rowSums(cts.t), decreasing = TRUE)
+    sums <- data.frame(sums)
+    sums$cond <- substr(rownames(sums), 0, 3)
+    sums <- sums[sums$cond == 'CON',]
+    samples_con <- rownames(sums)[1:5]
 
-deg <- res
-sig_deg = deg[ which(deg$padj < 0.1 & abs(deg$log2FoldChange) > log2(1.2)), ]
+    remove_samples <- unique(c(remove_samples, samples, samples_con))
+}
 
-num_up <- nrow(sig_deg[sig_deg$log2FoldChange > 0,])
-num_down <- nrow(sig_deg[sig_deg$log2FoldChange < 0,])
+if (!is.null(remove_samples)) {
+    print("removing samples...")
+    columns_to_remove <- grep(paste(remove_samples, collapse = "|"), names(counts_cellType))
+    print(columns_to_remove)
+    counts_cellType_filtered <- counts_cellType[-columns_to_remove]
+} else {
+    counts_cellType_filtered <- counts_cellType
+}
 
-EnhancedVolcano(deg,subtitle=NULL,
-                lab=rownames(deg),x='log2FoldChange',y='padj',pCutoff=0.1,labSize=4,FCcutoff=log2(1.2),
-                caption=glue("genes in at least 5% of cells = ", nrow(deg), "\n",
-                             "SIG UP = ", num_up, "\n", "SIG DOWN = ", num_down, "\n"),
-                legendLabels = c("NS", expression(log[2] ~ FC), "FDR", expression(FDR ~ and ~ log[2] ~ FC)))
-ggsave(glue('/home/ah2428/ShareZhangLab/ah2428/PTSD/deseq2_final/{celltype}_{cond}_volcano.png'))
+counts_cellType_filtered <- as.data.frame(counts_cellType_filtered)
 
+dge <- DGEList(counts = counts_cellType_filtered)
+cpm_matrix <- cpm(dge)
+cpm_percent <- rowSums(cpm_matrix<1) / ncol(cpm_matrix)  # % of samples that have <0.5 CPM value for each gene
+remove_genes <- names(cpm_percent[cpm_percent > 1])
+counts_cellType_filtered <- counts_cellType_filtered[!(rownames(counts_cellType_filtered) %in% remove_genes), ]
+
+# filter
+keep <- rowSums(counts_cellType_filtered) >= 20  ## PTSD: keeping only genes that have 20 reads and more
+counts_cellType_filtered <- counts_cellType_filtered[keep,]
+
+colData <- data.frame(samples = colnames(counts_cellType_filtered))
+colData <- colData %>%
+mutate(condition = ifelse(grepl(condition, samples), condition, 'CON')) %>%
+column_to_rownames(var = 'samples')
+colData$condition <- relevel(as.factor(colData$condition), 'CON')
+
+cov_meta <- unique(meta[, c('Channel','AgeDeath','PMI','RIN','Sex','Race','Condition')])
+rownames(cov_meta) <- paste0(cov_meta$Condition,'.',cov_meta$Channel)
+cov_meta <- cov_meta[ , !(names(cov_meta) %in% c('Channel','Condition'))]
+cov_meta$AgeDeath <- as.numeric(cov_meta$AgeDeath)
+cov_meta$PMI <- as.numeric(cov_meta$PMI)
+cov_meta$RIN <- as.numeric(cov_meta$RIN)
+cov_meta$Sex <- relevel(as.factor(cov_meta$Sex), 'M')
+cov_meta$Race <- relevel(as.factor(cov_meta$Race), 'W')
+
+colData <- merge(colData, cov_meta, by = 'row.names', all.y=FALSE)
+rownames(colData) <- colData$Row.names
+colData <- colData[ , !(names(colData) %in% c('Row.names'))]
+
+dds <- DESeqDataSetFromMatrix(countData = counts_cellType_filtered,
+                              colData = colData,
+                              design = ~AgeDeath + PMI + RIN + Sex + Race + condition)
+
+# run DESeq2
+dds <- DESeq(dds, 
+             test = "LRT",
+             reduced = ~AgeDeath + PMI + RIN + Sex + Race)
+res <- results(dds, name = glue("condition_{condition}_vs_CON"))
+sorted_res <- as.data.frame(res[order(res$pvalue, decreasing = FALSE), ])
+sorted_res_sig <- sorted_res[(!is.na(sorted_res$padj)) & (sorted_res$padj < 0.05), ]
+sorted_res_sig <- sorted_res_sig[(sorted_res_sig$log2FoldChange > 0.263) | (sorted_res_sig$log2FoldChange < -0.263), ]
+sorted_res_sig$geneName <- row.names(sorted_res_sig)
+write.table(sorted_res, glue('./DEG/{condition}-{celltype}_DEG.csv'),sep=',')
